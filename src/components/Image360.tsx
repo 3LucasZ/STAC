ecb3651efc3747f909635cbfe5dd1ed2ed2738b3
3LucasZ <lucas.zheng@warriorlife.net>
2024-12-23 03:40:48 -0800

    making the 360 image look better?

--- src/components/Image360.tsx
+++ src/components/Image360.tsx
@@ -2,7 +2,14 @@
 
 import { Canvas, useLoader } from "@react-three/fiber";
 
-import { BackSide, TextureLoader } from "three";
+import {
+  BackSide,
+  EquirectangularReflectionMapping,
+  LinearFilter,
+  LinearMipMapLinearFilter,
+  SRGBColorSpace,
+  TextureLoader,
+} from "three";
 import { Suspense } from "react";
 import { ActionIcon, AspectRatio, Box, Modal } from "@mantine/core";
 import { OrbitControls } from "@react-three/drei";
@@ -12,6 +19,7 @@ import {
   IconArrowsDiagonalMinimize,
   IconArrowsMinimize,
 } from "@tabler/icons-react";
+import { sRGBEncoding } from "@react-three/drei/helpers/deprecated";
 
 type Props = {
   url: string;
@@ -20,11 +28,14 @@ type Props = {
 export default function Image360(props: Props) {
   const [opened, { open, close }] = useDisclosure(false);
   const texture = useLoader(TextureLoader, props.url);
+  texture.mapping = EquirectangularReflectionMapping;
+  texture.colorSpace = SRGBColorSpace;
+  texture.generateMipmaps = false;
+  texture.minFilter = LinearFilter;
   const component = (
-    <Canvas camera={{ position: [0, 0, 0.1] }}>
+    <Canvas camera={{ position: [0, 0, 0.1], zoom: 0.75 }}>
       <OrbitControls
-        enableZoom={false}
-        minAzimuthAngle={-0.5}
+        minAzimuthAngle={-1}
         maxAzimuthAngle={1.5}
         enablePan={false}
         enableDamping
@@ -34,8 +45,8 @@ export default function Image360(props: Props) {
         rotateSpeed={0.5}
       />
       <Suspense fallback={null}>
-        <mesh>
-          <sphereGeometry attach="geometry" args={[500, 60, 40]} />
+        <mesh scale={[-100, 100, 100]}>
+          <sphereGeometry attach="geometry" args={[4, 60, 30]} />
           <meshBasicMaterial attach="material" map={texture} side={BackSide} />
         </mesh>
       </Suspense>
